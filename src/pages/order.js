0b36874b15dab5e71929d4d2ec0630e8de636e0b
Mohammed Sohail <itscrazybob@gmail.com>
2020-10-11 22:38:05 +0300

    Initial commit at the end of video 6

---
+++ src/pages/order.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function OrderPage() {
+  return <>This is the order page!</>;
+}
---
+++ src/pages/slicemasters.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function SliceMastersPage() {
+  return <>This is the slice masters page!</>;
+}
